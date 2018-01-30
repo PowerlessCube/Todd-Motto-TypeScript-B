b008d835b1a00cb7034acccf5abba4843117bc7c
PowerlessCube <alistair.mackay@escrivo.com>
2018-01-30 17:30:58 +0000

    inital commit

---
+++ playground/restParametersES6.ts
@@ -0,0 +1,9 @@
+// function sumAll(message, ...arr) {
+//   console.log(message);
+//   return arr.reduce((prev, next) => prev + next);
+// }
+
+// const sum = sumAll("Hello!", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+// console.log(sum);
+
